36dfe9bc037ee40847b5c4bd6c06df3b3b3ef97f
alfanmask <alfanh123123@gmail.com>
2022-07-29 15:13:43 +0700

    setup CI & Bootstrap

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+class Home extends CI_Controller{
+
+    public function __construct(){
+        parent::__construct();
+
+        // load something
+
+    }
+
+    public function index(){
+        $this->load->view('templates/header');
+        $this->load->view('home');
+        $this->load->view('templates/footer');
+
+    }
+
+
+}
