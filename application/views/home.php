f559a9494756ce526e95e6920503d83ba1fac298
alfanmask <alfanh123123@gmail.com>
2022-08-02 11:34:30 +0700

    frontend navbar and carousel

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,73 @@
-<h1 class="text-center">Hello World!</h1>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="<?= base_url('assets/img/logo-sociofarm.png') ?>" alt="" height="24">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Proyek</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Keunggulan</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mitra
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Dropdown 1</a></li>
+            <li><a class="dropdown-item" href="#">Dropdown 2</a></li>
+            <li><a class="dropdown-item" href="#">Dropdown 3</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kontak</a>
+        </li>
+      </ul>
+      <ul class="nav-right">
+        <li class="nav-item">
+            <a class="nav-link link-register" href="#">Daftar</a>
+        </li>
+        <li class="nav-item" style="float: right;">
+            <a class="nav-link link-login" href="#">Masuk</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-<p>HEHEHEHEH</p>
+<!-- carousel -->
+<div id="carouselHome" class="container carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselHome" data-slide-to="1"></li>
+    <li data-target="#carouselHome" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?= base_url('assets/img/slide1.png') ?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?= base_url('assets/img/slide2.png') ?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?= base_url('assets/img/slide3.png') ?>" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+  <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,11 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- FONTS -->
+    <style>@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@500&family=Montserrat:wght@400;600;700&display=swap');</style>
+    <script src="https://kit.fontawesome.com/a6b535fb5d.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
 
     <title>Hello, world!</title>
   </head>
